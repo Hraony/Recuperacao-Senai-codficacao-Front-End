0cc277e9475a1239f699fa571fc9c775442df556
Hraony <145581248+Hraony@users.noreply.github.com>
2024-01-01 22:31:03 -0300

    script atualizado alert
    Alerta de login bem sucedido adicionado

--- rec_frontend/script.js
+++ rec_frontend/script.js
@@ -3,6 +3,7 @@ const senhaInput = document.getElementById('senha')
 
 function login() {
     if(emailInput.value == 'admin@email.com' && senhaInput.value == '1234') {
+        alert('Logado com sucesso')
         window.location.href = "painel.html"
     } else {
         alert('email e/ou senha incorretos')
